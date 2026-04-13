c9e80b81591f32d1a0d8a04048302fe158591bff
Tah56 <tanzimahmed575@gmail.com>
2026-04-14 02:16:51 +0600

    add Total friend section and add img link in the json file

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import KeepFriends from '../../component/KeepsFriendBar/KeepFriends';
+import TotalFriendSection from '../../component/TotalFriendSection/TotalFriendSection';
 
 const HomePage = () => {
     return (
         <div className='bg-[#F8FAFC] py-20'>
            <KeepFriends/>
+           <TotalFriendSection/>
         </div>
     );
 };
